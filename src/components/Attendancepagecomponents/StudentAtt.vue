05419adf521d71f4e7a2cad342c1b8316d88ca3e
killvish-69 <asha.nina@gmail.com>
2023-02-27 16:53:17 +0530

    Attendence done but date conversion is not done yet

--- src/components/Attendancepagecomponents/StudentAtt.vue
+++ src/components/Attendancepagecomponents/StudentAtt.vue
@@ -1,33 +1,78 @@
-<template>
-    <div class="outerBlock">   
-        <div v-for="Att in Attendance" :key="Att.id">
-            {{ Att.subject }}
-        </div>
-    </div>
-</template>
+
 
 <script setup>
 import { defineProps } from 'vue';
 import { collection, getDocs,getDoc, where, query, doc } from '@firebase/firestore'
 import { db } from '../firebase'
-import { ref } from 'vue';
+import { ref,watchEffect } from 'vue';
+import { async } from '@firebase/util';
 
 const Attendance=ref([])
 const props=defineProps({
     FingerPrint:String,
+    subjects: String
 })
+let SelectedSubject=props.subjects
 let fID=props.FingerPrint
-const q =query(collection(db,"2022-2023"),where("fid","==",`${fID}`))
-getDocs(q).then((querySnapshot)=>{
- querySnapshot.forEach((doc) => {
+watchEffect(async () =>{
+    const AttendanceQuery =query(collection(db,"2022-2023"),where("fid","==",`${fID}`),where("subject","==",`${SelectedSubject}`))
+ const querySnapshotAttendance=await getDocs(AttendanceQuery)
+ querySnapshotAttendance.forEach((doc) => {
   Attendance.value.push({id:doc.id,...doc.data()})
+  console.log(Attendance.value.id)
  })
 })
 
+let present="p"
+let absent="a"
+
 </script>
 
+<template>
+    <div class="outerBlock">   
+        <div class="block" v-for="Att in Attendance" :key="Att.id">
+            <div class="attendance">
+                <span class="date">{{ Att.time.seconds }}</span>
+                <span v-if="Att.status == present" class="present">{{ Att.status }}</span>
+                <span v-if="Att.status == absent" class="absent">{{ Att.status }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
 <style scoped>
-.outerBlock{
-    background-color: pink;
+.block{
+    padding: 1rem 1rem;
+}
+.present{
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 20px;
+    padding:0.5rem 0.75rem;
+    height: 1rem;
+    width:1rem;
+    background-color:#7FE95A;
+    border:1px solid white;
+    border-radius: 100px;
+    margin-left: 1rem;
+    color: darkgreen;
+    -webkit-text-stroke: 1px teal;
+}
+.absent{
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 20px;
+    padding:0.5rem 0.75rem;
+    height: 1rem;
+    width:1rem;
+    background-color:E34242;
+    border:1px solid white;
+    border-radius: 100px;
+    margin-left: 1rem;
+    color: darkred;
+    -webkit-text-stroke: 1px teal;
+}
+.date{
+    color: white;
 }
 </style>
