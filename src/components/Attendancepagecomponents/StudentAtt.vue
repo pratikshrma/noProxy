351d9da3cc8a079700347d674d10d08ed1c8dda4
killvish-69 <asha.nina@gmail.com>
2023-02-28 16:28:37 +0530

    date logic modified

--- src/components/Attendancepagecomponents/StudentAtt.vue
+++ src/components/Attendancepagecomponents/StudentAtt.vue
@@ -14,31 +14,32 @@ const props = defineProps({
 })
 let SelectedSubject = props.subjects
 let fID = props.FingerPrint
-function formatDate(date) {
-    const formatDate = new Date(
-        date.seconds * 1000 + date.nanoseconds / 1000000
-    );
-    return formatDate.toLocaleTimeString('en-us', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-}
+// function formatDate(date) {
+//     const formatDate = new Date(
+//         date.seconds * 1000 + date.nanoseconds / 1000000
+//     );
+//     return formatDate.toLocaleTimeString('en-us', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+// }
 watchEffect(async () => {
     const AttendanceQuery = query(collection(db, "2022-2023"), where("fid", "==", `${fID}`), where("subject", "==", `${SelectedSubject}`))
     const querySnapshotAttendance = await getDocs(AttendanceQuery)
     querySnapshotAttendance.forEach((doc) => {
-        var dta = {
-            id: doc.id,
-            ...doc.data()
-        }
-        dta = {
-            ...dta,
-            time: formatDate(dta.time)
-        }
-        console.log(dta)
-        // Attendance.value.push({ id: doc.id, ...doc.data() })
-        Attendance.value.push(dta)
+        var timestamp=doc.data().time
+        var datetime=new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000)
+        const DateString=datetime.toLocaleDateString()
+        // var dta = {
+        //     id: doc.id,
+        //     ...doc.data()
+        // }
+        // dta = {
+        //     ...dta,
+        //     time: formatDate(dta.time)
+        // }
+        
+        Attendance.value.push({ id: doc.id,date:DateString, ...doc.data() })
+        // Attendance.value.push(dta)
     })
 })
-
-const i = "hello"
 
 let present = "p"
 let absent = "a"
@@ -49,7 +50,7 @@ let absent = "a"
     <div class="outerBlock">
         <div class="block" v-for="Att in Attendance" :key="Att.id">
             <div class="attendance">
-                <span class="date">{{ Att.time.seconds }}</span>
+                <span class="date">{{ Att.date }}</span>
                 <span v-if="Att.status == present" class="present">{{ Att.status }}</span>
                 <span v-if="Att.status == absent" class="absent">{{ Att.status }}</span>
             </div>
@@ -60,39 +61,31 @@ let absent = "a"
 <style scoped>
 .block {
     padding: 1rem 1rem;
+   
 }
-
+.attendance{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
 .present {
     text-transform: uppercase;
-    text-align: center;
     font-size: 20px;
-    padding: 0.5rem 0.75rem;
-    height: 1rem;
-    width: 1rem;
+    height: 2rem;
+    width: 2rem;
     background-color: #7FE95A;
     border: 1px solid white;
     border-radius: 100px;
     margin-left: 1rem;
     color: darkgreen;
     -webkit-text-stroke: 1px teal;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-.absent {
-    text-transform: uppercase;
-    text-align: center;
-    font-size: 20px;
-    padding: 0.5rem 0.75rem;
-    height: 1rem;
-    width: 1rem;
-    background-color: E34242;
-    border: 1px solid white;
-    border-radius: 100px;
-    margin-left: 1rem;
-    color: darkred;
-    -webkit-text-stroke: 1px teal;
-}
 
 .date {
+    font-size: 15px;
     color: white;
-}
-</style>
+}</style>
