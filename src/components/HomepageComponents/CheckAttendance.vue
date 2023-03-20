e7f6198944dc9103c5638aa02d717a9ba678f41c
Aditya <ashanina.shop@gmail.com>
2023-03-20 22:27:42 +0530

    Layout done! I hope it's done

--- src/components/HomepageComponents/CheckAttendance.vue
+++ src/components/HomepageComponents/CheckAttendance.vue
@@ -3,6 +3,7 @@
         <div ref="blackBG" @click="closeMenu" class="blackscreen"></div>
         <div class="attendanceMenu">
             <div ref="showMenu" class="DiffSemesters">
+                <span class="menuHeading">Please select a Semester :</span>
                 <div class="semester" v-for="sem in sems" :key="sem.id">
                     <RouterLink :to="`/attendance/${sem.id}`" class="routetoAttendence">
                         <span class="semText">{{ sem.name }}</span>
@@ -16,6 +17,8 @@
 
 <script setup>
 import { ref } from 'vue';
+import router from '../../router'
+import { watch } from 'vue';
 const sems = [
     { name: "1st sem", id: 1 },
     { name: "2nd sem", id: 2 },
@@ -23,7 +26,6 @@ const sems = [
     { name: "4th sem", id: 4 },
     { name: "5th sem", id: 5 },
     { name: "6th sem", id: 6 }
-
 ]
 const showMenu = ref()
 const blackBG = ref()
@@ -97,13 +99,12 @@ const closeMenu = () => {
 .attendance {
     height: 3.5rem;
     width: 4rem;
+    display: none;
 
 }
 
 .attendance:hover {
-    border: 1px solid white;
-    scale: 1.1;
-    box-shadow: 0px 0px 7px white;
+    scale: 1.2;
     cursor: pointer;
     transition: .2s ease-in-out;
 }
@@ -119,28 +120,38 @@ const closeMenu = () => {
     }
 }
 
+.menuHeading {
+    display: none;
+}
+
 @media screen and (max-width:890px) {
 
     .DiffSemesters {
         position: absolute;
         flex-direction: column;
+        width: 50%;
         top: -1rem;
         justify-content: flex-start;
-        gap: 0.5rem;
-        left: 50%;
-        background-color: lavender;
-        padding-top: 5rem;
+        gap: 3rem;
+        right: 0;
+        background-color: teal;
+        padding: 5rem 1rem;
         height: 44.5rem;
         display: none;
+
     }
 
-    .semester:hover {
-        scale: 1;
+    .menuHeading {
+        color: white;
+        display: block;
     }
+
 
     .semText {
-        color: black;
-        border-radius: 0px;
+        color: white;
+        border-radius: 10px;
+        padding: 1rem .5rem;
+        /* border: 1px double white; */
         border: none;
     }
 
@@ -149,16 +160,23 @@ const closeMenu = () => {
     }
 
     .semText:hover {
-        background: teal;
-        color: white;
+        scale: 1;
+        border: none;
+        background-color: rgb(255, 255, 255, 0.5);
+        border-bottom: 3px solid white;
         transition: .2s ease-in-out;
+    }
+
+    .semester:hover {
+        scale: 1;
+        padding: 0 0;
     }
 
     .attendance {
         display: flex;
         position: relative;
         font-size: .9rem;
-        padding: 0 1rem;
+        padding: 0 0;
         margin-left: -1rem;
     }
 
